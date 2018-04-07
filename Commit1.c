dab287694dd98be6557ea243061adb9940332a05
raghavs17 <37906309+raghavs17@users.noreply.github.com>
2018-04-07 20:33:45 +0530

    Priority Based Assignment of Processes to Queue

--- Commit1.c
+++ Commit1.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
-struct process {
+struct process
+{
 		int priority;
 		int id;
 		int burst;
 		int arrival;
 		int response;
 };
+int r1=-1,r2=-1,r3=-1;
+int f1=-1,f2=-1,f3=-1;
+struct process q1[100], q2[100], q3[100] ;
 void create(int n, struct process p[])
 {	
-	for(int i=0; i<n; i++)
+	int i;
+	for(i=0; i<n; i++)
 	{
 		printf("\n\nFor process : %d \n",i+1);
 		printf("\n\nEnter Process ID : ") ;
@@ -25,7 +30,8 @@ void create(int n, struct process p[])
 }
 void show(int n, struct process p[])
 {
-	for(int i=0; i<n; i++)
+	int i;
+	for(i=0; i<n; i++)
 	{
 		printf("\n\nProcess ID : ") ;
 		printf("%d\n",p[i].id) ;
@@ -37,6 +43,68 @@ void show(int n, struct process p[])
 		printf("%d\n\n",p[i].priority) ;
 	}
 }
+void pushInq1(struct process p)
+{
+	if(f1 ==-1)
+	{
+		f1=0 ;
+	}
+	r1 = r1+1;
+	q1[r1] = p;
+}
+void pushInq2(struct process p)
+{
+	if(f2 == -1) {
+		f2 = 0 ;
+	}
+	r2 = r2 + 1 ;
+	q2[r2] = p ;
+}
+void pushInq3(struct process p)
+{
+	if(f3 == -1) {
+		f3 = 0 ;
+	}
+	r3 = r3 + 1 ;
+	q3[r3] = p ;
+}
+void putinQueue(int n, struct process p[])
+{
+	int i ;
+	for(i=0; i<n; i++)
+	{
+		if((p[i].priority)>=1 && (p[i].priority)<20)
+		{
+			pushInq1(p[i]) ;
+		}
+		else if ((p[i].priority)>=20 && (p[i].priority)<40)
+		{
+			pushInq2(p[i]) ;
+		}
+		else if ((p[i].priority)>=40 && (p[i].priority)<60)
+		{
+			pushInq3(p[i]) ;
+		} 
+	}	
+}
+void displayQueue()
+{
+	int i ;
+	printf("Queue 1 : ") ;
+	for(i=0;i<=r1;i++) {
+		printf("P%d  ",q1[i].id) ;
+	}
+	printf("\nQueue 2 : ") ;
+	for(i=0;i<=r2;i++) {
+		printf("P%d  ",q2[i].id) ;
+	}
+	printf("\nQueue 3 : ") ;
+	for(i=0;i<=r3;i++) {
+		printf("P%d  ",q3[i].id) ;
+	}
+	printf("\n") ;
+}
+
 int main()
 	{
 	int n ;
@@ -47,4 +115,6 @@ int main()
 	create(n,p) ;
 	printf("Displaying %d processes : \n",n);
 	show(n,p);
+	putinQueue(n,p);
+	displayQueue();
 	}
