3847f7a5829ab1b600ab3130e5e500c0d842b3ed
raghavs17 <37906309+raghavs17@users.noreply.github.com>
2018-04-08 23:20:47 +0530

    Used Round Robin with Quanta=4 on 1st Queue(Highest Priority Processes)

--- Commit.c
+++ Commit.c
@@ -91,20 +91,60 @@ void displayQueue()
 {
 	int i ;
 	printf("Queue 1 : ") ;
-	for(i=0;i<=r1;i++) {
+	for(i=0;i<=r1;i++)
+	{
 		printf("P%d  ",q1[i].id) ;
 	}
 	printf("\nQueue 2 : ") ;
-	for(i=0;i<=r2;i++) {
+	for(i=0;i<=r2;i++)
+	{
 		printf("P%d  ",q2[i].id) ;
 	}
 	printf("\nQueue 3 : ") ;
-	for(i=0;i<=r3;i++) {
+	for(i=0;i<=r3;i++)
+	{
 		printf("P%d  ",q3[i].id) ;
 	}
 	printf("\n") ;
 }
-
+void rRobin(int n, struct process p[])
+{
+	int i,j,time,remaining=n,flag=0,quanta = 4,wTime=0,taTime=0 ;
+	
+	printf("\n\nProcess\t\tWaiting Time    Turnaround Time\n\n"); 
+  	for(time=0,i=0;remaining!=0;)
+	  {  	 
+	    if((p[i].response<=quanta) && (p[i].response)>0)
+		{ 
+			time+=(p[i].response); 
+	      	(p[i].response)=0; 
+	      	flag=1; 
+	    }
+		else if((p[i].response)>0)
+		{ 
+			(p[i].response)-=quanta; 
+			time+=quanta; 
+	    }
+		if((p[i].response)==0 && flag==1)
+		{ 
+		    remaining--; 
+			printf("Process[%d]\t\t%d\t\t%d\n",p[i].id,time-((p[i].arrival)-(p[i].burst)),time-(p[i].arrival)); 
+		    wTime+=time-(p[i].arrival)-(p[i].burst); 
+			taTime+=time-(p[i].arrival); 
+		    flag=0; 
+	    }
+		if(i==n-1) 
+	    	i=0; 
+	    else if((p[i+1].arrival)<=time) 
+	    	i++; 
+	    else 
+	    	i=0; 
+	}
+}
+void s1()
+{
+	rRobin(r1+1, q1) ;
+}
 int main()
 	{
 	int n ;
@@ -117,4 +157,5 @@ int main()
 	show(n,p);
 	putinQueue(n,p);
 	displayQueue();
+	s1();
 	}
